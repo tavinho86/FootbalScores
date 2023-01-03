a6ab6d4153057935d0efb7902df363fbe3d05186
Otavio Henrique Pereira <35305534+tavinho86@users.noreply.github.com>
2023-01-03 19:09:47 -0300

    Add files via upload

---
+++ FootballScores/Business.cs
@@ -0,0 +1,50 @@
+﻿namespace FootballScores
+{
+    public class Business
+    {
+        public int[] Calculate(List<int> teamA, List<int> teamB)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int goalsB in teamB)
+            {
+                int count = 0;
+
+                foreach (int goalsA in teamA)
+                {
+                    if (goalsA <= goalsB)
+                    {
+                        count++;
+                    }
+                }
+
+                result.Add(count);
+            }
+
+            return result.ToArray();
+        }
+
+        public int[] CalculateBetter(List<int> teamA, List<int> teamB)
+        {
+            List<int> result = new List<int>();
+            Dictionary<int, int> keyValueTeamA = new Dictionary<int, int>();
+
+            int index = 0;
+            foreach (int goalsA in teamA)
+            {
+                keyValueTeamA.Add(index, goalsA);
+
+                index++;
+            }
+
+            foreach (int goalsB in teamB)
+            {
+                int count = keyValueTeamA.Count(goalsA => goalsA.Value <= goalsB);
+
+                result.Add(count);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ FootballScores/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FootballScores;
+using System.Runtime.CompilerServices;
+
+//TODO: Tratar erro de conversão de número em todos os ReadLine
+
+Console.WriteLine("Enter the number of matches for Team A");
+
+int sizeTeamA = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Enter the number of goals in matches for Team A, one per line");
+
+List<int> goalsTeamA = new List<int>();
+
+for (int i = 0; i < sizeTeamA; i++)
+{
+    goalsTeamA.Add(int.Parse(Console.ReadLine()));
+}
+
+Console.WriteLine();
+Console.WriteLine("Enter the number of matches for Team B");
+int sizeTeamB = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Enter the number of goals in matches for Team B, one per line");
+
+List<int> goalsTeamB = new List<int>();
+
+for (int i = 0; i < sizeTeamB; i++)
+{
+    goalsTeamB.Add(int.Parse(Console.ReadLine()));
+}
+
+Console.WriteLine();
+Console.WriteLine("Calculating the result");
+Console.WriteLine();
+
+//int[] result = new Business().Calculate(goalsTeamA, goalsTeamB);
+int[] result = new Business().CalculateBetter(goalsTeamA, goalsTeamB);
+
+Console.WriteLine($"Result = [ { string.Join(", ", result) } ]");
